fc9475a462d8b61f2656a10b17ce6c1f4999a742
jsunnyge <jsunnyge@volvocars.com>
2024-05-28 11:24:48 +0200

    chore: Update Minikube startup in setup.sh and add sparkUIOptions in spark-pi.yaml

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
-
-helm repo add spark-operator https://googlecloudplatform.github.io/spark-on-k8s-operator
+if minikube status >/dev/null 2>&1; then
+    echo "Minikube is already running."
+else
+    echo "Starting Minikube..."
+    minikube start || { echo "Failed to start Minikube. Please make sure Minikube is installed."; exit 1; }
+    minikube status | grep "Running" >/dev/null 2>&1
+    while [ $? -ne 0 ]; do
+        echo "Waiting for Minikube to be ready..."
+        sleep 5
+        minikube status | grep "Running" >/dev/null 2>&1
+    done
+fi
+helm repo add spark-operator https://kubeflow.github.io/spark-operator
 helm repo update
-helm install my-release spark-operator/spark-operator --namespace spark-operator --create-namespace --set sparkJobNamespace=default
+helm install my-release spark-operator/spark-operator --namespace spark-operator --create-namespace --set sparkJobNamespace=default --set 'image.repository=ghcr.io/googlecloudplatform/spark-operator' --set 'image.tag=v1beta2-1.4.3-3.5.0'
 kubectl apply -f spark-pi.yaml
